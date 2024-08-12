f342c69ea187a563a1c1d90977016b322267b5e1
t0nqwer <oottoonnoo@gmail.com>
2024-08-13 00:57:48 +0700

    refactor: Update PORT variable to use process.env.PORT or 5050
    The code changes modify the `PORT` variable in the `index.js` file to use `process.env.PORT` as the default value, or `5050` if the environment variable is not set. This change ensures that the server listens on the correct port, providing flexibility for deployment environments.

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
