be15e20bdf91663ef28b704f5681482de19c3681
t0nqwer <oottoonnoo@gmail.com>
2024-08-16 00:08:39 +0700

    feat: Add price fields for different currencies to ProductModal.js
    The code changes in `ProductModal.js` add new price fields (`priceTHB`, `priceUSD`, `priceEUR`, `priceAED`) to the `ProductSchema`. This enhancement allows storing and retrieving product prices in different currencies, providing flexibility for international pricing.

--- models/Product/ProductModal.js
+++ models/Product/ProductModal.js
@@ -9,6 +9,10 @@ const ProductSchema = new Schema({
   designcode: { type: Schema.Types.ObjectId, ref: "DesignCode" },
   status: { type: String, required: true, default: "active", enum: ["active", "inactive", "draft"] },
   detail: { type: Array, required: true, default: [] },
+  priceTHB: { type: Number, required: true },
+  priceUSD: { type: Number, required: true },
+  priceEUR: { type: Number, required: true },
+  priceAED: { type: Number, required: true },
 });
 
 const Product = model("Product", ProductSchema);
